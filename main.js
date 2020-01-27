ad1e74179017c598e1330053820d4b6f05961477
Abdul Rehman <mehars.6925@gmail.com>
2020-01-27 11:18:11 +0500

    socket.io setup

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+const http = require('http');
 const express = require('express');
-const bodyParser = require("body-parser");
+const socket = require('socket.io');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-const { PORT } = require('./config/vars');
 const authRoutes = require('./routes/auth');
 const connection = require('./config/connection');
+const { PORT, SECRET } = require('./config/vars');
 const protectedRoutes = require('./routes/protected');
 
 connection.query('use woochat'); 
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,4 +29,91 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(PORT);
+
+server.listen(PORT);
+
+// Socket.io 
+
+let onlineUsers = [];
+let pendingMessages = [];
+
+io.use((socket, next) => {
+  if (socket.handshake.query && socket.handshake.query.token) {
+    const token = socket.handshake.query.token;
+    jwt.verify(token, SECRET, (error, payload) => {
+      if (error) return next(new Error("Socket.io Authentication Error Occured."));
+      socket.request.user = payload;
+    });
+    next();
+  } else {
+      return next(new Error("Socket.io Authentication Error Occured."));
+  }
+});
+
+io.on('connection', (socket) => {
+
+  let user = {
+    socket: socket.id,
+    user: socket.request.user
+  };
+
+  if (!onlineUsers.includes(user)) onlineUsers.push(user);
+
+
+  // Lets Send Pending Messages
+
+  pendingMessages.forEach((message, idx) => {
+    let reciever = onlineUsers.filter(u => u.user._id == message.reciever)[0];
+    if (reciever) {
+      io.to(reciever.socket).emit(message);
+      pendingMessages.splice(idx, 1);
+    }
+  });
+
+
+  socket.on('message', (data) => {
+    if (data.message && data.reciever && socket.request.user.id != data.reciever) {
+      let reciever = onlineUsers.filter(
+        u => u.user._id == data.reciever
+      )[0];
+
+      connection.query(
+        'INSERT INTO messages(sender_id, receiver_id, message) VALUES (?, ?, ?)',
+        [
+          socket.request.user.id,
+          data.reciever,
+          data.message
+        ],
+        (err, res) => {
+          if (err) {
+            socket.emit('notification', {
+              success: false,
+              message: 'Internal Server Error'
+            });
+          } else {
+            const message = {
+              sender: socket.request.user.id,
+              reciever: data.reciever,
+              message: data.message
+            };
+
+            if (reciever) {
+              io.to(reciever.socket).emit('message', message);
+            } else {
+              pendingMessages.push(message);
+            }
+
+          }
+
+        }
+      )
+    }
+  });
+
+  socket.on('disconnect', () => {
+    onlineUsers = onlineUsers.filter(
+      o => o.socket !== socket.id
+    );
+  });
+
+});
